1bfc8f041d0f461774b08201b7bba7f8bf57e41f
Omisola Idowu Paul <57432464+omisolaidowu@users.noreply.github.com>
2023-01-18 23:36:34 +0100

    Vercel deploy update

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -81,22 +81,22 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'null_project',
-#         'USER': 'postgres',
-#         'PASSWORD': 'cmosbattery',
-#         'HOST': 'localhost',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR+"/"+'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'null_project',
+        'USER': 'postgres',
+        'PASSWORD': 'cmosbattery',
+        'HOST': 'localhost',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR+"/"+'db.sqlite3',
+#     }
+# }
 
 
 
