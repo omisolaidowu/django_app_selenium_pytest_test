aeb3541e99ba42ae5f9d0f9052a44a9e058466f7
Omisola Idowu Paul <57432464+omisolaidowu@users.noreply.github.com>
2023-01-19 10:50:42 +0100

    PostgreSQL Password hidden

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,17 @@
 from django.contrib.auth.models import User
 import pytest
+import os
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+
 
 @pytest.fixture
 def superAdmin() -> User:
     User.objects.create_user(
          'admin', 
          'omisolaidowu@gmail.com,',
-       'cmosbattery',
+       os.getenv('SUPER_ADMIN_PASSWORD'),
        is_superuser=True
        )
 
